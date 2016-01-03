bbbe9d5d41e9caa9c4e7e1fe22f0f195f2d4f378
Brian Gasberg Thomsen <mail@briangasberg.dk>
2016-01-03 12:30:55 +0100

    bgt

--- exam_plan/sos/create_files.sh
+++ exam_plan/sos/create_files.sh
@@ -5,12 +5,12 @@ do
     file="mm${i}.tex"
     if [ -f "$file" ]
     then
-	echo "$file already exists"
+	echo "$file already exists, no action performed."
     else
 	echo "$file created."
-        touch "mm${i}.tex"
-        echo "\chapter{${i}}" > mm${i}.tex
-        echo "\input{mm${i}.tex}" >> main.tex
+        touch "${file}"
+        echo "\chapter{${i}}" > ${file}
+        echo "\input{${file}}" >> main.tex
         echo "$file added to main.tex"
     fi
 done
