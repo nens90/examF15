82bea31c827be1c583e70a04fb1f5ae79482fb0b
Brian Gasberg Thomsen <mail@briangasberg.dk>
2016-01-03 12:27:16 +0100

    bgt

--- exam_plan/sos/create_files.sh
+++ exam_plan/sos/create_files.sh
@@ -2,7 +2,15 @@
 
 for i in {1..11}
 do
-    touch "mm${i}.tex"
-    echo "\chapter{${i}}" > mm${i}.tex
-    echo "\input{mm${i}.tex}" >> main.tex
+    file="mm${i}.tex"
+    if [ -f "$file" ]
+    then
+	echo "$file already exists"
+    else
+	echo "$file created."
+        touch "mm${i}.tex"
+        echo "\chapter{${i}}" > mm${i}.tex
+        echo "\input{mm${i}.tex}" >> main.tex
+        echo "$file added to main.tex"
+    fi
 done
